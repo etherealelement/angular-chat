58512afbae9797dccd95aa76c25c4b7800ede28a
etherealelement <vitalik.safonov@list.ru>
2024-10-07 06:01:59 +0300

    add auth

--- src/app/modules/auth/ui/auth.component.ts
+++ src/app/modules/auth/ui/auth.component.ts
@@ -5,6 +5,7 @@ import { ButtonComponent } from '../../../shared/ui-kit/button/button.component'
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AuthService} from '../services/auth.service';
 import {from, map} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -15,17 +16,18 @@ import {from, map} from 'rxjs';
 })
 export class AuthComponent {
   authService = inject(AuthService)
+  router = inject(Router)
 
   form = new FormGroup({
-    username: new FormControl(null, Validators.required),
-    password: new FormControl(null, Validators.required),
+    username: new FormControl<string | null>(null, Validators.required),
+    password: new FormControl<string | null>(null, Validators.required),
   });
 
   onSubmit(event: Event) {
     event.preventDefault()
     if (this.form.valid) {
       // @ts-ignore
-      this.authService.login(this.form.value);
+      this.authService.login(this.form.value)
     }
   }
 }
