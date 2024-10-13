e845c82b24f5c9c05e8138cd430fcce2260912e5
etherealelement <vitalik.safonov@list.ru>
2024-10-13 14:55:40 +0300

    add: profile info ui

--- src/app/modules/profile-info/model/profile-info.component.ts
+++ src/app/modules/profile-info/model/profile-info.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { SvgIconComponent } from '../../../shared/ui/svg-icon/svg-icon.component';
+import { HeadingComponent } from '../../../shared/ui-kit/heading/heading.component';
+import { ProfileTagComponent } from '../../../shared/ui/profile-tag/profile-tag.component';
 
 @Component({
     selector: 'app-profile-info',
     standalone: true,
-    imports: [SvgIconComponent],
+    imports: [SvgIconComponent, HeadingComponent, ProfileTagComponent],
     templateUrl: '../ui/profile-info.component.html',
     styleUrl: '../ui/profile-info.component.scss',
 })
