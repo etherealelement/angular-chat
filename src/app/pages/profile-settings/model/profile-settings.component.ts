8475dc5f56a8623b94071e402b243eb82ab89124
etherealelement <vitalik.safonov@list.ru>
2024-10-20 16:14:30 +0300

    fix: settings footer

---
+++ src/app/pages/profile-settings/model/profile-settings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-ui',
+    standalone: true,
+    imports: [],
+    templateUrl: '../ui/profile-settings.component.html',
+    styleUrl: '../ui/profile-settings.component.scss',
+})
+export class ProfileSettingsComponent {}
